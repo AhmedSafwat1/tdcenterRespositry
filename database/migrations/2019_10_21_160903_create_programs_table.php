fec159560252dd6a477c09410df9a4fac69d1418
mo <muhammadelkhateb95@gmail.com>
2019-10-22 15:27:18 +0200

    schema001

---
+++ database/migrations/2019_10_21_160903_create_programs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProgramsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('programs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('description');
+            $table->timestamps();
+
+            $table->biginteger('module_id')->unsigned();
+            $table->foreign('module_id')->references('id')->on('modules');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('programs');
+    }
+}
---
+++ database/migrations/2019_10_21_163846_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->datetime('event_start');
+            $table->datetime('event_end');
+            $table->integer('capacity');
+            $table->string('location');
+            $table->timestamps();
+
+            $table->biginteger('program_id')->unsigned();
+            $table->foreign('program_id')->references('id')->on('programs');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
